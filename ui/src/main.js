5e5ee452640b407e4cd5a8f47af64480fdb9e21b
George Petrakis <g.petrakis@natechbanking.com>
2025-06-13 15:07:50 +0300

    Fix: correct import statement for router in main.js

--- ui/src/main.js
+++ ui/src/main.js
@@ -4,7 +4,7 @@
  */
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router' // This now imports from the /router directory
+import router from './router'
 
 // Import Vuetify and styles
 import 'vuetify/styles'
